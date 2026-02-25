430249d287c19d4fdf6c17eb5cf426426ac475e0
Nikita Shepovalov <shepovalov.nikita@mail.ru>
2026-02-25 10:12:53 +0700

    Добавлены оросители

--- main/urls.py
+++ main/urls.py
@@ -13,10 +13,11 @@ urlpatterns = [
     # path("", index),
     path("sprinkler/", include("sprinkler.urls")),
     # path('', views.index, name='index'),
-    path('', views.new_view, name='new'),
+    path('ayp/', views.new_view, name='new'),
     path('editor/', views.editor_view, name='editor'),
     path("api/", api.urls),
     path("users/", include("users.urls", namespace="users")),
+    path("ipa/", include("python.urls")),
 
 ]
 
