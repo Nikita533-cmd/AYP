34ef3242a9da03188ae871e82c061d83c5173ca2
Березиков СА <ik15@sauto22.ru>
2026-02-25 11:58:12 +0700

    WIP: настройки base.py перед переключением

--- main/settings/base.py
+++ main/settings/base.py
@@ -158,9 +158,11 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR.parent.parent / "media"
 
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR.parent.parent / "static"
 STATICFILES_DIRS = [
     BASE_DIR.parent / "static",
 ]
+
